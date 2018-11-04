02d7b02ebb773a3fa01278fe644f2de38326833b
Daniel Klock <danielkl@stud.ntnu.no>
2018-11-04 13:06:43 +0100

    Add Word entity.

---
+++ app/src/main/java/no/daniel/hangman/data/entity/Word.java
@@ -0,0 +1,19 @@
+package no.daniel.hangman.data.entity;
+
+import android.support.annotation.NonNull;
+
+import no.daniel.hangman.util.Language;
+
+public class Word implements Entity {
+    public final String content;
+    public final Language language;
+
+    public Word(@NonNull String word, Language language) {
+        content = cleanWord(word, language.specChars);
+        this.language = language;
+    }
+
+    private static String cleanWord(@NonNull String word, String specialCharacters) {
+        return word.trim().toLowerCase().replaceAll("[-_]", " ").replaceAll("[^a-z " + specialCharacters + "]", "");
+    }
+}
